b2a755dfdb820450f14afb02343ff1e8d812b6c4
spidernz <spidernz@ihug.co.nz>
2013-08-13 11:53:37 +1200

    new Score manager

---
+++ WordFlash/Classes/Model/WFScoreManager.h
@@ -0,0 +1,13 @@
+//
+//  WFScoreManager.h
+//  WordFlash
+//
+//  Created by Tony Newth on 13/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WFScoreManager : NSObject
+
+@end
---
+++ WordFlash/Classes/Model/WFScoreManager.m
@@ -0,0 +1,13 @@
+//
+//  WFScoreManager.m
+//  WordFlash
+//
+//  Created by Tony Newth on 13/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import "WFScoreManager.h"
+
+@implementation WFScoreManager
+
+@end
