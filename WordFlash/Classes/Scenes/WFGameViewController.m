81d919dac1b590fd724074ee0ee7750f3e9068b5
Matthew Seymour <mseymour.home@gmail.com>
2013-08-12 14:07:30 +1200

    stuff

--- WordFlash/Classes/Scenes/WFGameViewController.m
+++ WordFlash/Classes/Scenes/WFGameViewController.m
@@ -23,11 +23,32 @@
     return self;
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+//	_motionManager = [[CMMotionManager alloc] init];
+//	if (_motionManager.deviceMotionAvailable)
+//	{
+//		_motionManager.deviceMotionUpdateInterval = 1.0/60.0;
+//		
+//		NSOperationQueue *queue = [NSOperationQueue mainQueue];
+//		[_motionManager startDeviceMotionUpdatesToQueue:queue withHandler:^(CMDeviceMotion *motion, NSError *error)
+//		 {
+//			 _deviceMotion = motion;
+//			 
+//		 }];
+//		
+//	} else{
+//		NSLog(@"no motion");
+//	}
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-
+	UIButton *testButton = [UIButton buttonWithTitle:@"test" target:self selector:nil];
+	[self.view addSubview:testButton];
 	
 }
 
