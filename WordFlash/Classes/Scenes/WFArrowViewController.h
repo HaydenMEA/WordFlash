b03f1cbaaf0644ce6d83de2163cab14e333eb4ea
Matthew Seymour <mseymour.home@gmail.com>
2013-08-12 12:49:21 +1200

    Made Arrow View Controller For the Arrow

---
+++ WordFlash/Classes/Scenes/WFArrowViewController.h
@@ -0,0 +1,13 @@
+//
+//  WFArrowViewController.h
+//  WordFlash
+//
+//  Created by Matthew Seymour on 12/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFArrowViewController : UIViewController
+
+@end
---
+++ WordFlash/Classes/Scenes/WFArrowViewController.m
@@ -0,0 +1,38 @@
+//
+//  WFArrowViewController.m
+//  WordFlash
+//
+//  Created by Matthew Seymour on 12/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import "WFArrowViewController.h"
+
+@interface WFArrowViewController ()
+
+@end
+
+@implementation WFArrowViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
