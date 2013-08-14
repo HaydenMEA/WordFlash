fdca32317ce2776dda59ab873bf28bc358256c7a
Hayden <i_of_me@hotmail.com>
2013-08-15 09:51:45 +1200

    storyboard hack

---
+++ WordFlash/Classes/UI/WFScrollButton.h
@@ -0,0 +1,13 @@
+//
+//  WFScrollButton.h
+//  WordFlash
+//
+//  Created by Hayden Young on 15/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFScrollButton : UIButton
+
+@end
