00b3787024280c79298f6fc31f3353734b0cec36
Matthew Seymour <mseymour.home@gmail.com>
2013-08-14 14:53:52 +1200

    added backgrounds

--- WordFlash/Classes/Scenes/WFScoreViewController.m
+++ WordFlash/Classes/Scenes/WFScoreViewController.m
@@ -36,6 +36,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+	UIImage *image = iPhone568Image(@"background.png");//[UIImage imageNamed:@"background.png"];
+	self.view.backgroundColor = [UIColor colorWithPatternImage:image];
 	_scoreManager = [WFScoreManager defaultManager];
 	_wordController = [WFWordController defaultManager];
 	
